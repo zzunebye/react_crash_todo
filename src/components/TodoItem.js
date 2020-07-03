5c3d44fe5de61719666c20f9ac86fad86bd1fdf9
BangJun Young <bhang96@gmail.com>
2020-07-03 13:36:54 +0900

    done

---
+++ src/components/TodoItem.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    }
+  }
+
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+          {title}
+          {/*  에러가 생기는 이유는 delTodo 프롭을 만들지 않았기 때문이다. */}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
+        </p>
+      </div>
+    )
+  }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
+}
+
+// Style
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 9px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
+}
+
+export default TodoItem
