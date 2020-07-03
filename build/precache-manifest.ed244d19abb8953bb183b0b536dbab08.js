f7d9fb617af27bd0eb25891c4c47a5053a1dd4c4
BangJun Young <bhang96@gmail.com>
2020-07-03 13:38:55 +0900

    first commit

---
+++ build/precache-manifest.ed244d19abb8953bb183b0b536dbab08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0194340fce7c55effbef18a64d0269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "892e363556b872bbab74",
+    "url": "/static/css/main.fbf1e897.chunk.css"
+  },
+  {
+    "revision": "4be6026bdd04e6d73a4d",
+    "url": "/static/js/2.a19d3369.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.a19d3369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892e363556b872bbab74",
+    "url": "/static/js/main.61a9e79e.chunk.js"
+  },
+  {
+    "revision": "f8ce95e23ae4eb434ece",
+    "url": "/static/js/runtime-main.ef835f46.js"
+  }
+]);
