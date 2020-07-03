a551f71d49f327d98b946e945c65d6253a39ec1f
BangJun Young <bhang96@gmail.com>
2020-07-03 20:29:23 +0900

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class AddTodo extends Component {
   state = {
-    title: ''
+    title: "",
     // title에 대한 정보는 부모 컴포넌트가 아닌 이 컴포넌트에 존재, but this is not an acutal component-level.
-  }
+  };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
@@ -14,15 +14,15 @@ export class AddTodo extends Component {
 
     this.props.addTodo(this.state.title); // 부모 컴포넌트 <App>이 해당 함수를 프롭으로 넘겨주었다는 전제. 함수는 <App>에서 콜한다.
     this.setState({ title: "" });
-  }
+  };
 
   render() {
     return (
-      <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
+      <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
         <input
           type="text"
           name="title"
-          style={{ flex: '10', padding: '5px' }}
+          style={{ flex: "10", padding: "5px" }}
           placeholder="Add Todo..."
           value={this.state.title}
           onChange={this.onChange}
@@ -31,16 +31,15 @@ export class AddTodo extends Component {
           type="submit"
           value="Submit"
           className="btn"
-          style={{ flex: '1' }}
+          style={{ flex: "1" }}
         />
       </form>
-    )
+    );
   }
 }
 // PropTypess
 AddTodo.propTypes = {
   addTodo: PropTypes.func.isRequired,
-}
+};
 
-
-export default AddTodo
+export default AddTodo;
